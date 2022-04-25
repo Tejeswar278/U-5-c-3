383a5c307072e862bfa7a5e67eef871f531ae2e2
Muddada Tejeswara Rao <lsandptlove143@gmail.com>
2022-04-25 16:55:58 +0530

    completed

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,3 +1,7 @@
+import { useEffect, useState } from "react";
+import axios, { Axios } from "axios";
+import '../App.css'
+
 export const Home = () => {
     // create statistics for user.
     // get Total user count from DB,
@@ -11,22 +15,36 @@ export const Home = () => {
     //   promoted: 0,// inc when user in promoted
     //   total_new: 0,// inc when a new user in created
   
+    const [totalemploys , setemploy] = useState(0);
+    const [totalterm , setemployterm] = useState(0);
+    const [totalpromoted , setemploypro] = useState(0);
+    const [totalnew , setemploynew] = useState(0);
+
+    useEffect(() =>{
+      fetachEmpTotal(); 
+    },[]);
+
+    const fetachEmpTotal = async () => {
+      const {data} = await axios ("http://localhost:8080/employee");
+      setemploy(data.length);
+    }
+
     return (
       <>
         <h3 className="welcome">Welcome To employee management system</h3>
         <div className="home">
           <span>Stats</span>
-          <div>
-            Total Employees<span className="totalemp"></span>
+          <div className="list backg">
+            Total Employees: <span className="totalemp">{totalemploys}</span>
           </div>
-          <div>
-            Total Terminated: <span className="total_terminated"></span>
+          <div className="list">
+            Total Terminated: <span className="total_terminated">{totalterm}</span>
           </div>
-          <div>
-            Total Promoted: <span className="total_promoted"></span>
+          <div className="list backg">
+            Total Promoted: <span className="total_promoted">{totalpromoted}</span>
           </div>
-          <div>
-            Total New: <span className="total_new"></span>
+          <div className="list">
+            Total New: <span className="total_new">{totalnew}</span>
           </div>
         </div>
       </>
