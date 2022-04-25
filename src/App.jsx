64974ec09c6d96cb537703984afbec0fe335320c
Muddada Tejeswara Rao <lsandptlove143@gmail.com>
2022-04-25 14:49:09 +0530

    updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,43 +1,37 @@
 import { useState } from 'react'
 import logo from './logo.svg'
 import './App.css'
+import {Routes, Route} from 'react-router-dom';
+import {Home} from './components/Home';
+import {Login} from './components/Login';
+import {EmployeeList} from './components/Employeelist';
+import {EmployeeDetails} from './components/EmployeeDetails';
+import {Admin} from './components/Admin';
+import {Navbar} from './components/Navbar';
+import {Logout} from './components/Logout';
+import {ProtectRouter} from './components/Privateroute';
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Hello Vite + React!</p>
-        <p>
-          <button type="button" onClick={() => setCount((count) => count + 1)}>
-            count is: {count}
-          </button>
-        </p>
-        <p>
-          Edit <code>App.jsx</code> and save to test HMR updates.
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-          {' | '}
-          <a
-            className="App-link"
-            href="https://vitejs.dev/guide/features.html"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Vite Docs
-          </a>
-        </p>
-      </header>
+      <Navbar></Navbar>
+      <Routes>
+        <Route path='/' element={<Home></Home>}></Route>
+        <Route path='/login' element={<Login></Login>}></Route>
+        <Route path='/employee' element={<EmployeeList></EmployeeList>}></Route>
+        <Route 
+          path='/employees/:id'
+          element={
+            <ProtectRouter>
+              <EmployeeDetails></EmployeeDetails>
+            </ProtectRouter>
+          }
+          ></Route>
+          <Route path='/admin' element={<Admin></Admin>}></Route>
+          <Route path='/logout' element={<Logout></Logout>}></Route>
+      </Routes>
     </div>
   )
 }
