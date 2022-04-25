3aea1a242971f8e172155408b419ca9b5892faf4
Muddada Tejeswara Rao <lsandptlove143@gmail.com>
2022-04-25 14:59:44 +0530

    employ detials

--- src/components/EmployeeDetails.jsx
+++ src/components/EmployeeDetails.jsx
@@ -1,14 +1,35 @@
+import { useEffect, useState } from 'react';
+import axios from 'axios';
+import { useParams } from 'react-router-dom';
+
 export const EmployeeDetails = () => {
+    const [employ, setemploy] = useState({});
+
+    useEffect(()=> {
+        getemploy();
+    }, []);
+
+    const getemploy = async () => {
+        const {data} =  await axios(`http://localhost:8080/employ/${id}`);
+        setemploy(data);
+    }
+
+    const {id} = useParams();
+
     return (
       <div className="user_details">
-        <img className="user_image" />
-        <h4 className="user_name"></h4>
-        <span className="user_salary">$</span>
+        <img className="user_image" src={empoly.image}/>
+        <h4 className="user_name">{empoly.username}</h4>
+        <span className="user_salary">${empoly.salary}</span>
         <span className="tasks">
-          <li className="task"></li>
+            {empoly?.tasks?.map((t, i) => (
+                <li className="task" key={i}>
+                    {t}
+                </li>
+            ))}
         </span>
-        Status: <b className="status"></b>
-        Title: <b className="title"></b>
+        Status: <b className="status">{empoly.status}</b>
+        Title: <b className="title">{empoly.title}</b>
         {/* Show this button only if user is not already terminated (users status is working) */}
         <button className="fire">Fire Employee</button>
         {/* Show this button only if user is not already team lead or terminated */}
